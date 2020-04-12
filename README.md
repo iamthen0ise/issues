1df4cc23da7b20647d34b20ad4982327899b185c
kikisix <e.uvarov@me.com>
2020-04-13 00:12:12 +0300

    Increase issues limit. Add Readme.

---
+++ README.md
@@ -0,0 +1,5 @@
+0. Create Personal Access Token at https://github.com/settings/tokens
+1. Insert into `.env.example` file instead of placeholder
+2. Rename `env.example` into `.env`
+3. Run `yarn ` or `npm install`
+4. After installation process run `node main.js`
--- graphql/getIssuesByRepo.js
+++ graphql/getIssuesByRepo.js
@@ -4,7 +4,7 @@ let getIssuesByRepo = function (repo, owner) {
         repository(name: ${JSON.stringify(repo)}, owner: ${JSON.stringify(
     owner
   )}) {
-        issues(orderBy: {field: CREATED_AT, direction: DESC}, states: OPEN, first: 10) {
+        issues(orderBy: {field: CREATED_AT, direction: DESC}, states: OPEN, first: 100) {
             nodes {
             title
             url
